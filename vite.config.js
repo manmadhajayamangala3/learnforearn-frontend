91634fb79d01641624f55d41404aeef1ed250e95
Manmadha Jayamangala <manmadha.jayamangala@cloudfuze.com>
2026-06-23 15:06:00 +0530

    all isuues and fixes

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
+    minify: 'esbuild',
+    target: 'es2020',
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
   },
 })
